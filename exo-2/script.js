84b26e1cd23449fed084b703f9a556af6ae19564
LRTESC <escurelaurent@gmail.com>
2023-01-12 12:16:26 +0100

    correction exo 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,8 +1,53 @@
 // you can write js here
-console.log('exo-2');
+//console.log('exo-2');
 
+//const myDate = new Date;
+
+//const week = 'c est la semaine' ; 
+//const weekend = 'c est le weekend';
+
+//let day = parseInt(prompt("jour de la semaine"));
+
+//let hour = parseInt(prompt("heure de la journée"));
+
+    //if((day === 0 || day === 6) || (day === 5 && hour > 16) || (day === 1 && hour < 9)){
+    //console.log(weekend);
+    //}else{
+    //console.log(week);
+//}
+
+// you can write js here
 const myDate = new Date();
-const Day1 = myDate.getDay();
+
+let isTesting = true;
+
+const weekday = "jour de la semaine";
+const weekend = "top c est le weekend";
+
+let day;
+let hour;
+
+
+if (isTesting) {
+    day = parseInt(prompt("Entrer jour de la semaine (1-7)"));
+    hour = parseInt(prompt("Entrer une heure (0-23)"));
+} else {
+    day = myDate.getDay();
+    hour = myDate.getHours();
+}
+
+
+if ((day === 0 || day === 6) || (day === 5 && hour > 16 ) || (day === 1 && hour < 9)){
+    console.log(weekend);
+} else {
+    console.log(weekday);
+}
+
+
+
+
+
+
 
 
 
