d92d283a7c22aed1b9271bd66d8256113fd507b9
LRTESC <escurelaurent@gmail.com>
2023-01-16 10:49:02 +0100

    correction exo-4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,12 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop(); 
+secretMessage.push("to","program");
+secretMessage[6] = "right"; 
+secretMessage.shift();
+secretMessage.unshift("Programming");
+secretMessage.splice(5, 9, "know");
+
+console.log(secretMessage.join(''));
