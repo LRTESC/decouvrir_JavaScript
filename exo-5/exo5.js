fb515f6ec422290e1ce815688badc805855d8e70
LRTESC <escurelaurent@gmail.com>
2023-01-16 09:32:03 +0100

    correction exo-4

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,16 @@
 // you can write js here
 
 console.log('exo-5');
+
+const input = prompt("Que voulez vous traduire");
+const vowels = ["a", "e", "i", "o", "u", "y"];
+
+let resultArray = [];
+
+for (const letter of input) {
+    if (vowels.indexOf(letter) !== -1) {
+        resultArray.push(letter);
+    }
+}
+
+console.log(resultArray.join("").toUpperCase());
